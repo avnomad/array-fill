3254348ef8d009abb0f94ba4162c72fd8854799a
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-02-08 21:27:34 +0200

    Added old VS2008 project to mercurial. (after renaming it to 'array fill' from 'Benchmark array fill Forms')
    Original timestamp (from .sln):    8/11/2008 5:31:26pm
    Original timestamp (from Form1.h): 8/11/2008 5:32:12pm
    
    --HG--
    branch : C++/CLI Windows Forms

---
+++ array fill/Form1.h
@@ -0,0 +1,106 @@
+#pragma once
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+
+using std::ofstream;
+
+using std::setprecision;
+
+using std::fabs;
+
+using std::clock;
+
+namespace ArrayFill {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	///
+	/// WARNING: If you change the name of this class, you will need to change the
+	///          'Resource File Name' property for the managed resource compiler tool
+	///          associated with all .resx files this class depends on.  Otherwise,
+	///          the designers will not be able to interact properly with localized
+	///          resources associated with this form.
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(292, 273);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+				 	clock_t outTime , inTime;
+					ofstream outFile("c:/stats.txt");	// variables required for measuring and storing the results.
+
+					unsigned int size = 0;	// variables needed for the program itself.
+					double *A;
+
+					outTime = clock();
+
+					for(size = 1000000 ; size <= 50000000 ; size += 250000)
+					{
+						A = new double[size];
+						inTime = clock();		
+						for(int c = 0 ; c < size ; ++c)
+							A[c] = c;
+						outFile<<clock()-inTime<<endl;
+						delete[] A;
+					}
+
+					outFile<<"\n\n"<<clock()-outTime<<endl;
+			 }
+	};
+}
+
